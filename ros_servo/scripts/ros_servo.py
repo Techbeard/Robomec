b8d0aa5a3d81f7455c1b1a6ac95576edbaab0bf7
Vale <ddd>
2019-01-05 22:17:27 +0100

    funktioniert bis auf rückwärts

--- ros_servo/scripts/ros_servo.py
+++ ros_servo/scripts/ros_servo.py
@@ -52,14 +52,14 @@ def main():
 
 
     while not rospy.is_shutdown():
-        vel_tp[len(vel_tp)-1] = speed if not timeout > TIMEOUT else 0
+        vel_tp[len(vel_tp)-1] = speed #if not timeout > TIMEOUT else 0
         vel_tp[:-1] = vel_tp[1:]
 
         dir_tp[len(dir_tp)-1] = direction
         dir_tp[:-1] = dir_tp[1:]
 
-        tx_speed = (sum(vel_tp)/len(vel_tp))*1000
-        tx_dir = (sum(dir_tp)/len(dir_tp))*90000
+        tx_speed = (sum(vel_tp)/len(vel_tp))*18000
+        tx_dir = (sum(dir_tp)/len(dir_tp))*-90000
 
         rospy.loginfo("Speed: %f", tx_speed)
         rospy.loginfo("Steering: %f", tx_dir)
@@ -67,11 +67,14 @@ def main():
        #motorR = tx_speed + tx_dir
        #motorL= tx_speed - tx_dir
 
-        servo.set_velocity(0, 10000)
+        servo.set_degree(0, -9000, 9000)
+        
+        servo.set_velocity(0, 65535)
         servo.set_position(0, tx_dir)
         servo.enable(0)
        
-        servo.set_velocity(1, 10000)
+        servo.set_degree(1, -9000, 9000)
+        servo.set_velocity(1, 65535)
         servo.set_position(1, tx_speed)
         servo.enable(1)
 
